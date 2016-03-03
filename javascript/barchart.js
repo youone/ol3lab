461c30bd27f1be43f66d0dfb7b0ea951b09c4349
Johan Lundquist <jolundq@gmail.com>
2016-03-03 06:32:08 +0100

    added file

---
+++ javascript/barchart.js
@@ -0,0 +1,89 @@
+InitChart();
+
+function InitChart() {
+  var barData = [{
+    'x': 1,
+    'y': 5
+  }, {
+    'x': 20,
+    'y': 20
+  }, {
+    'x': 40,
+    'y': 10
+  }, {
+    'x': 60,
+    'y': 40
+  }, {
+    'x': 80,
+    'y': 5
+  }, {
+    'x': 100,
+    'y': 60
+  }];
+
+  var vis = d3.select('#visualisation')
+  
+    WIDTH = 1000;
+    HEIGHT = 500;
+    MARGINS = {
+      top: 20,
+      right: 20,
+      bottom: 20,
+      left: 50
+    },
+    xRange = d3.scale.ordinal().rangeRoundBands([MARGINS.left, WIDTH - MARGINS.right], 0.1).domain(barData.map(function (d) {
+      return d.x;
+    })),
+
+
+    yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0,
+      d3.max(barData, function (d) {
+        return d.y;
+      })
+    ]),
+
+    xAxis = d3.svg.axis()
+      .scale(xRange)
+      .tickSize(5)
+      .tickSubdivide(true)
+
+    yAxis = d3.svg.axis()
+      .scale(yRange)
+      .tickSize(5)
+      .orient("left")
+      .tickSubdivide(true)
+
+    var zoom = d3.behavior.zoom()
+    .x(x)
+    .y(y)
+    .scaleExtent([1, 100])
+    .on("zoom", zoomed);
+
+
+  vis.append('svg:g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
+    .call(xAxis);
+
+  vis.append('svg:g')
+    .attr('class', 'y axis')
+    .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
+    .call(yAxis);
+
+  vis.selectAll('rect')
+    .data(barData)
+    .enter()
+    .append('rect')
+    .attr('x', function (d) {
+      return xRange(d.x);
+    })
+    .attr('y', function (d) {
+      return yRange(d.y);
+    })
+    .attr('width', xRange.rangeBand())
+    .attr('height', function (d) {
+      return ((HEIGHT - MARGINS.bottom) - yRange(d.y));
+    })
+    .attr('fill', 'grey');
+
+}
